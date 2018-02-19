8cebeacc54e926f503e695094ee2c7aede66a306
davy <davy.wybiral@gmail.com>
2018-02-19 15:32:28 -0600

    added version number

--- cmd/hookah/main.go
+++ cmd/hookah/main.go
@@ -8,15 +8,20 @@ import (
 	"os"
 )
 
+const version = "0.1.0"
+
 func usage() {
 	fmt.Println("NAME:")
 	fmt.Println("   hookah - WebSocket pipeline tool\n")
 	fmt.Println("USAGE:")
 	fmt.Println("   hookah command [command options]\n")
+	fmt.Println("VERSION:")
+	fmt.Println("   " + version + "\n")
 	fmt.Println("COMMANDS:")
-	fmt.Println("   node		start new node")
-	fmt.Println("   recv		pipe from node to local stdout")
-	fmt.Println("   send		pipe local stdout to node")
+	fmt.Println("   node        start new node")
+	fmt.Println("   recv        pipe from node to local stdout")
+	fmt.Println("   send        pipe local stdout to node")
+	fmt.Println("   version     print current version")
 	fmt.Println("")
 }
 
@@ -33,6 +38,8 @@ func main() {
 		cmd_recv.Main(args[1:])
 	case "send":
 		cmd_send.Main(args[1:])
+	case "version":
+		fmt.Println(version)
 	default:
 		usage()
 		return
