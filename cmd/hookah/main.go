6dc8d99e687b93d52bb411bfa2214e077975164e
davy <davy.wybiral@gmail.com>
2018-06-30 12:26:53 -0500

    restructure CLI

--- cmd/hookah/main.go
+++ cmd/hookah/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -12,14 +13,33 @@ import (
 )
 
 func main() {
+	// Create hookah API instance
+	h := hookah.New()
+	flag.Usage = func() {
+		fmt.Print("NAME:\n")
+		fmt.Print("   hookah\n\n")
+		fmt.Print("USAGE:\n")
+		fmt.Print("   hookah -i input -o output\n\n")
+		fmt.Print("VERSION:\n")
+		fmt.Printf("   %s\n\n", hookah.Version)
+		fmt.Print("INPUTS:\n")
+		for _, reg := range h.ListInputs() {
+			fmt.Printf("   %s\n", reg.Usage)
+		}
+		fmt.Print("\n")
+		fmt.Print("OUTPUTS:\n")
+		for _, reg := range h.ListOutputs() {
+			fmt.Printf("   %s\n", reg.Usage)
+		}
+		fmt.Print("\n")
+		os.Exit(0)
+	}
 	// Parse flags
 	var inOpts string
 	flag.StringVar(&inOpts, "i", "stdin", "Stream input")
 	var outOpts string
 	flag.StringVar(&outOpts, "o", "stdout", "Stream output")
 	flag.Parse()
-	// Create hookah API instance
-	h := hookah.New()
 	// Setup input stream
 	r, err := h.NewInput(inOpts)
 	if err != nil {
