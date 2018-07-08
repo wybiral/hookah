0d7c3d5fea7472154157a741c1d2e3a4020b63e6
davy <davy.wybiral@gmail.com>
2018-07-08 00:25:30 -0500

    fix race

--- internal/app/app.go
+++ internal/app/app.go
@@ -52,17 +52,16 @@ func New(h *hookah.API, config *Config) (*App, error) {
 
 // Run the App and block until complete or Quit channel received.
 func (a *App) Run() (err error) {
+	// Closing this channel will stop readerloops and writerloop
+	done := make(chan struct{})
 	go func() {
-		defer func() {
-			recover()
-		}()
 		err = <-a.Quit
-		close(a.Ch)
+		close(done)
 	}()
 	for _, n := range a.Readers {
-		go a.readerloop(n)
+		go a.readerloop(n, done)
 	}
-	er := a.writerloop()
+	er := a.writerloop(done)
 	if er != nil {
 		err = er
 	}
@@ -143,15 +142,16 @@ func (a *App) createW(opts []string) error {
 // Called in a goroutine for each reader node. Continuously calls Read on the
 // Node and sends the []byte (wrapped in an Update) to App.Ch. Failure here
 // should signal on App.Quit.
-func (a *App) readerloop(n node.Node) {
-	defer func() {
-		recover()
-	}()
+func (a *App) readerloop(n node.Node, done chan struct{}) {
 	for {
 		b := make([]byte, a.Config.BufferSize)
 		i, err := n.Read(b)
 		if i > 0 {
-			a.Ch <- &Update{Bytes: b[:i], Source: n}
+			select {
+			case a.Ch <- &Update{Bytes: b[:i], Source: n}:
+			case <-done:
+				return
+			}
 		}
 		// EOF shouldn't really be an error but it should exit
 		if err == io.EOF {
@@ -168,8 +168,14 @@ func (a *App) readerloop(n node.Node) {
 
 // Receive updates from App.Ch and Write out to all App.Writers. Doesn't Write
 // to the Node if it's the source of the Update (to avoid loops).
-func (a *App) writerloop() error {
-	for u := range a.Ch {
+func (a *App) writerloop(done chan struct{}) error {
+	var u *Update
+	for {
+		select {
+		case u = <-a.Ch:
+		case <-done:
+			return nil
+		}
 		for _, n := range a.Writers {
 			// Don't send to self
 			if u.Source == n {
@@ -181,5 +187,4 @@ func (a *App) writerloop() error {
 			}
 		}
 	}
-	return nil
 }
--- internal/app/config.go
+++ internal/app/config.go
@@ -1,7 +1,9 @@
 package app
 
+// DefaultBufferSize is default internal buffer size for readers.
 const DefaultBufferSize = 8 * 1024
 
+// Config options for App.
 type Config struct {
 	RWOpts     []string
 	ROpts      []string
